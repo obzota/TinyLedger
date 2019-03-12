de2fc4c8a288d79450f66a0088f62305dba08456
Remy Rey <remy.rey@netlight.com>
2019-03-12 09:51:01 +0100

    Initial Commit

---
+++ TinyLedger/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  TinyLedger
+//
+//  Created by Remy Rey on 2019-03-12.
+//  Copyright © 2019 Remy Rey. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
